406043256d85223eb2114c02faadb21d6964f7e4
asteroidice <ryan.s.rabello@gmail.com>
2016-08-31 22:47:46 -0700

    Added lazy load to search

--- app/routes/search/search.component.ts
+++ app/routes/search/search.component.ts
@@ -17,10 +17,13 @@ export class SearchComponent implements OnActivate {
   query: string;
   year: string;
   profile_results: ProfileModel[] = [];
+  partial_results: ProfileModel[] = [];
+  profileIndex: number = 0;
 
   constructor( curr: RouteSegment, private req: RequestService ) {
     this.query = curr.getParam('query');
     this.year = curr.getParam('year') || CURRENT_YEAR;
+
   }
 
   routerOnActivate() {
@@ -29,7 +32,17 @@ export class SearchComponent implements OnActivate {
         r.year = this.year;
         return new ProfileModel(r);
       });
+      this.showMore();
       console.log(this.profile_results);
     }, null);
+  }
+
+  showMore(){
+    for (var i = 0; i <= 11; i++) {
+      if(this.profile_results[this.profileIndex]){
+        this.partial_results.push(this.profile_results[this.profileIndex]);
+        this.profileIndex++;
+      }
+    }
   }
 }
